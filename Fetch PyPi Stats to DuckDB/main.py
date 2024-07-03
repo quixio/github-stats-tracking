c7d14610e6bedda2cef52a7646df38cad064af5f
Merlin The Great <merlin+newdemo@quix.io>
2024-07-03 20:48:32 +0000

    Edited 'main.py'

--- Fetch PyPi Stats to DuckDB/main.py
+++ Fetch PyPi Stats to DuckDB/main.py
@@ -8,37 +8,42 @@ load_dotenv()
 mdtoken = os.environ['MOTHERDUCK_TOKEN']
 mddatabase = os.environ['MOTHERDUCK_DATABASE']
 targetable = os.environ['TABLE_NAME']
-
-# More docs: https://github.com/hugovk/pypistats#numpy-and-pandas
-df = pypistats.overall("quixstreams", total=True, format="pandas")
-
-print(df.head())
-
-print(f"Connecting to {mddatabase}...")
-
-# initiate the MotherDuck connection through a service token through
-conn = duckdb.connect(f'md:{mddatabase}?motherduck_token={mdtoken}')
+sleeptime = int(os.environ["sleeptime"])
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-# Create a table from the DataFrame
-conn.execute(f'''
-CREATE TABLE IF NOT EXISTS {targetable} (
-    category VARCHAR,
-    date DATE,
-    percent VARCHAR,
-    downloads INTEGER
-);
-''')
+while True:
+    # More docs: https://github.com/hugovk/pypistats#numpy-and-pandas
+    df = pypistats.overall("quixstreams", total=True, format="pandas")
 
-# Insert data from the DataFrame into the table
-conn.register('df_view', df)
-conn.execute(f'INSERT INTO {targetable} SELECT * FROM df_view')
+    print(df.head())
 
-# Verify the data insertion
-result = conn.execute(f'SELECT * FROM {targetable}').fetchall()
-print(result)
+    print(f"Connecting to {mddatabase}...")
 
-# Close the connection
-conn.close()
+    # initiate the MotherDuck connection through a service token through
+    conn = duckdb.connect(f'md:{mddatabase}?motherduck_token={mdtoken}')
+
+    # Create a table from the DataFrame
+    conn.execute(f'''
+    CREATE TABLE IF NOT EXISTS {targetable} (
+        category VARCHAR,
+        date DATE,
+        percent VARCHAR,
+        downloads INTEGER
+    );
+    ''')
+
+    # Insert data from the DataFrame into the table
+    conn.register('df_view', df)
+    conn.execute(f'INSERT INTO {targetable} SELECT * FROM df_view')
+
+    # Verify the data insertion
+    result = conn.execute(f'SELECT * FROM {targetable}').fetchall()
+    print(result)
+
+    # Close the connection
+    conn.close()
+
+    print(f"All rows published. Sleeping {sleeptime} secs ({sleeptime / 3600} hours)...")
+    time.sleep(sleeptime) # sleep 1 hour
